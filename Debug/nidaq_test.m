0f5c5e190bbce91424f95e3c974ce47f0ef917cb
Stephen Zhang <x.stephen.zhang@gmail.com>
2022-03-24 17:22:43 -0400

    updated PCB

--- Debug/nidaq_test.m
+++ Debug/nidaq_test.m
@@ -12,3 +12,13 @@ end
 
 daq_connection.addDigitalChannel(daqname, 'Port0/Line0:3', 'InputOnly');
 daq_connection.stop;
+
+%% Triggered
+daqname = 'Dev2';
+daq_connection = daq.createSession('ni'); %create a session in 64 bit
+daq_connection.Rate = 1000; 
+ai = daq_connection.addAnalogInputChannel(daqname, 0:7 , 'Voltage'); 
+daq_connection.addDigitalChannel(daqname, 'Port0/Line0:3', 'InputOnly');
+daq_connection.startForeground;
+
+% s.addTriggerConnection('Dev1/PFI4','Dev2/PFI0','StartTrigger')
