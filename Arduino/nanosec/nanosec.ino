0b20f3f7963693ee2b87351df4fdec241050e5f9
Stephen Zhang <x.stephen.zhang@gmail.com>
2025-09-01 15:41:51 -0400

    Update nanosec.ino

--- Arduino/nanosec/nanosec.ino
+++ Arduino/nanosec/nanosec.ino
@@ -48,7 +48,7 @@
 //
 
 // =============== Version ===============
-#define nsver "v3.55"
+#define nsver "v3.56"
 
 // =============== Hardware ===============
 // Now running teensy 4.0 at 450 Mhz (2.2 ns step)
